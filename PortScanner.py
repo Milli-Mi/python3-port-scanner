408a0a6a50f408969a96ba8fbc7f13959c6b5ba5
Milli-Mi <87952989+Milli-Mi@users.noreply.github.com>
2022-08-28 16:04:11 +0300

    Update PortScanner.py

--- PortScanner.py
+++ PortScanner.py
@@ -1,4 +1,4 @@
-#!/bin/python3
+
 
 import socket
 
@@ -6,12 +6,12 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 host = '137.74.187.102'
 
-port = 443
+port = 80
 
 def portScanner(port):
-  if s.connect_ex((host, port)):
-    print("Port is closed")
-  else: 
-    print("Port is open")
+    if s.connect_ex((host, port)):
+        print("The port is closed")
+    else:
+        print("The port is open")
 
-    portScanner(port) 
+portScanner(port)
