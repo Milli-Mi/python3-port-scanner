40615b6abfae4577e492f4a613512be10f81ab33
Milli-Mi <87952989+Milli-Mi@users.noreply.github.com>
2022-08-29 12:31:03 +0300

    Update PortScanner.py

--- PortScanner.py
+++ PortScanner.py
@@ -4,9 +4,9 @@ import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-host = '137.74.187.102'
+host = input("Please enter the IP you want to  scan: ")
 
-port = 80
+port = int(input("Please enter the port you want to scan: "))
 
 def portScanner(port):
     if s.connect_ex((host, port)):
